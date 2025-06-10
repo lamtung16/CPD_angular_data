924841daa9d8b126d321b59950cc8619ef65bb80
lamtung16 <nguyenlamtung10@gmail.com>
2025-06-10 17:59:25 +0200

    add apart

--- utils.py
+++ utils.py
@@ -4,6 +4,11 @@ pi = np.pi
 np.random.seed(123)
 
 
+# geodesic distance
+def geo_d(theta, psi):
+    return np.sum(np.square(np.minimum(np.abs(psi - theta), 2*pi - np.abs(psi - theta))).reshape(-1, theta.shape[1]), axis=1)
+
+
 # get cumsum matrix of signal given Theta
 def get_cumsum(signal, Theta):
     costs = np.empty((len(signal), len(Theta)))
@@ -13,7 +18,7 @@ def get_cumsum(signal, Theta):
 
 
 # get loss value of the segment
-def L(start, end, cumsum_costs):
+def L_segment(start, end, cumsum_costs):
     return np.min(cumsum_costs[end] - cumsum_costs[start], axis=1)
 
 
@@ -39,7 +44,7 @@ def get_signal_mean(Theta, cumsum_costs, chpnts):
 
 
 # get changpoints and signal_mean by optimal partitiniong
-def opart(signal, Theta, lda):
+def pelt(signal, Theta, lda):
     cumsum_costs = get_cumsum(signal, Theta)                    # get cumsum_costs matrix
     
     # Get tau_star (list of best last changepoints)
@@ -49,7 +54,7 @@ def opart(signal, Theta, lda):
 
     candidates = np.array([0])
     for t in range(1, len(signal) + 1):
-        V = C[candidates] + lda + L(candidates, t, cumsum_costs)      # calculate set V
+        V = C[candidates] + lda + L_segment(candidates, t, cumsum_costs)      # calculate set V
         best_idx = np.argmin(V)
         tau_star[t] = candidates[best_idx]
         C[t] = V[best_idx]
@@ -61,3 +66,15 @@ def opart(signal, Theta, lda):
     signal_mean = get_signal_mean(Theta, cumsum_costs, chpnts)  # get signal means given set of changepoints
     chpnts = chpnts[1:-1] - 1
     return chpnts, signal_mean
+
+
+def apart(signal, Theta, lda):
+    theta_star = -1.0 * np.ones(shape=(len(signal) + 1, Theta.shape[1]))
+    V = np.zeros(len(signal) + 1)
+    for t in range(1, len(signal) + 1):
+        V_candidates = V[t-1] + geo_d(Theta, signal[t-1]) + lda * np.any(theta_star[t - 1] != Theta, axis=1)
+        best_idx = np.argmin(V_candidates)
+        theta_star[t] = Theta[best_idx]
+        V[t] = V_candidates[best_idx]
+    chpnts = np.arange(len(signal) - 1)[np.any(theta_star[2:] != theta_star[1:-1], axis=1)]
+    return chpnts, theta_star[1:]
